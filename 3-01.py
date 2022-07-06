76b6aa2e353b18e924599e5c0f6a1f400b21077f
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-07-06 12:30:01 +0500

    change 3-01.py

--- 3-01.py
+++ 3-01.py
@@ -1,9 +1,5 @@
 from Levenshtein import distance
 
-sourse = 'cat'
-dest = 'kitty'
-dist = distance(sourse, dest)
-print('sourse:', sourse, ', dest:', dest, '; distance =', dist)
 if __name__ == '__main__':
     pairs = [
         ('home', 'work'),
