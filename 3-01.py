aea642a981b4090d4f16916f9088e55b3a1ce39d
Евгения Костарева <eugenia@mac-book-air-2020.local>
2022-07-06 12:34:44 +0500

    change 3-01.py

--- 3-01.py
+++ 3-01.py
@@ -1,12 +1,13 @@
 from Levenshtein import distance
 
+pairs = [
+    ('home', 'work'),
+    ('home', 'house'),
+    ('computer', 'computers'),
+    ('house', 'townhouse'),
+]
+
 if __name__ == '__main__':
-    pairs = [
-        ('home', 'work'),
-        ('home', 'house'),
-        ('computer', 'computers'),
-        ('house', 'townhouse'),
-    ]
     for sourse, dest in pairs:
         dist = distance(sourse, dest)
         print('sourse:', sourse, ', dest:', dest, '; distance =', dist)
